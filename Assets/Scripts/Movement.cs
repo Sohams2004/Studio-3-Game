71e5b8cbb93d02327c81b708f82be5a0ab7e0c0b
Soham Shinde <sohamsshinde04@gmail.com>
2024-06-07 14:39:27 +0400

    Player movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10f;
+
+    [SerializeField] private Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float inputx = Input.GetAxis("Horizontal");
+        float inputz = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = (transform.forward * inputz + transform.right * inputx) * movementSpeed * 100 * Time.deltaTime;
+        playerRb.velocity = new(moveDirection.x, playerRb.velocity.y, moveDirection.z);
+    }
+}
