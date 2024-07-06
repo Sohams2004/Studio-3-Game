7f032c9bab004b4381a3787c73c68ba6e5669069
Aly Sharaf <ali.charaf59@gmail.com>
2024-07-06 20:39:42 +0400

    "F" to drink orange juice (text is next to juice after it spawns) + sound

--- Assets/Ali/Scripts/PickUpPlace.cs
+++ Assets/Ali/Scripts/PickUpPlace.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickUpPlace : MonoBehaviour
 {
-   private GameObject pickedObject;
+  private GameObject pickedObject;
     public float pickUpRange = 2.0f;
     public float holdDistance = 0.7f;
     public LayerMask pickUpLayer;
+
+    private GameObject orangeJuice;
+    public TextMeshPro drinkText;
+    public AudioSource drinkSound;
+
+    private bool isLookingAtOrangeJuice = false;
 
     void Update()
     {
@@ -26,6 +33,13 @@ public class PickUpPlace : MonoBehaviour
         if (pickedObject != null)
         {
             HoldObject();
+        }
+
+        CheckForInteraction();
+
+        if (isLookingAtOrangeJuice && Input.GetKeyDown(KeyCode.F))
+        {
+            DrinkOrangeJuice();
         }
     }
 
@@ -56,5 +70,40 @@ public class PickUpPlace : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         pickedObject.transform.position = ray.GetPoint(holdDistance);
+    }
+
+    void CheckForInteraction()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, pickUpRange, pickUpLayer))
+        {
+            if (hit.collider.gameObject.CompareTag("OrangeJuice"))
+            {
+                orangeJuice = hit.collider.gameObject;
+                drinkText.transform.position = orangeJuice.transform.position + new Vector3(0, 1, 0);
+                drinkText.gameObject.SetActive(true);
+                isLookingAtOrangeJuice = true;
+            }
+            else
+            {
+                drinkText.gameObject.SetActive(false);
+                isLookingAtOrangeJuice = false;
+            }
+        }
+        else
+        {
+            drinkText.gameObject.SetActive(false);
+            isLookingAtOrangeJuice = false;
+        }
+    }
+
+    void DrinkOrangeJuice()
+    {
+        Destroy(orangeJuice);
+        drinkText.gameObject.SetActive(false);
+        drinkSound.Play();
+        isLookingAtOrangeJuice = false;
     }
 }
