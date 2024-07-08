36defc901566d95e1331b9daeb01d2a9495c7ba7
Aly Sharaf <ali.charaf59@gmail.com>
2024-07-08 19:09:17 +0400

    PlayerWakeUp camera animation completed

---
+++ Assets/Ali/AnimationController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public GameObject playerObject; // Assign your player object in the Inspector
+
+    void Start()
+    {
+        // Disable the player object initially
+        playerObject.SetActive(false);
+    }
+
+    public void OnAnimationFinished()
+    {
+        // This method is called when your animation finishes playing
+        playerObject.SetActive(true);
+    }
+}
