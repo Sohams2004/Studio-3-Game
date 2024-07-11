c66fb28330cc5d16426b5c16e82a497e1d25e478
AlexIdris <146952700+AlexIdris@users.noreply.github.com>
2024-07-11 14:38:34 +0400

    Reapply "Sprinting and ontrigger revert"
    This reverts commit c7e4e400a3cf0387c36488517b918e17666cc31d.

--- Assets/Ali/Scripts/Interactable.cs
+++ Assets/Ali/Scripts/Interactable.cs
@@ -26,7 +26,7 @@ public class Interactable : MonoBehaviour
 
     void Update()
     {
-        /*if (playerInRange && Input.GetKeyDown(KeyCode.E) && !hasInteracted)
+        if (playerInRange && Input.GetKeyDown(KeyCode.E) && !hasInteracted)
         {
             objectAnimator.SetBool("Blindsup", true);
             objectAnimator.SetBool("Blindsdown", false);
@@ -40,11 +40,10 @@ public class Interactable : MonoBehaviour
             hasInteracted = false;
 
             audioSource.PlayOneShot(interactionSound);
-        }*/
-        PlayBlindAnim();
+        }
     }
 
-    void PlayBlindAnim()
+    /*void PlayBlindAnim()
     {
         if (objectPickUp.isBlinds)
         {
@@ -72,9 +71,9 @@ public class Interactable : MonoBehaviour
                 audioSource.PlayOneShot(interactionSound);
             }
         }
-    }
+    }*/
 
-    /*void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
@@ -120,5 +119,5 @@ public class Interactable : MonoBehaviour
             playerInRange = false;
 
         }
-    }*/
+    }
 }
