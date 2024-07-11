c7e4e400a3cf0387c36488517b918e17666cc31d
AlexIdris <146952700+AlexIdris@users.noreply.github.com>
2024-07-11 14:37:34 +0400

    Revert "Sprinting and ontrigger revert"
    This reverts commit 97a9301186704ab89aa9bf2e8f542f85782a9e5f.
    
    # Conflicts:
    #	Assets/Scenes/Skeleton Layout/Home Layout 1.unity

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -22,16 +22,6 @@ public class Movement : MonoBehaviour
 
         Vector3 moveDirection = (transform.forward * inputz + transform.right * inputx) * movementSpeed * 100 * Time.deltaTime;
         playerRb.velocity = new(moveDirection.x, playerRb.velocity.y, moveDirection.z);
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            movementSpeed *= 1.5f;
-        }
-
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            movementSpeed = 10f;
-        }
     }
 
     private void Update()
