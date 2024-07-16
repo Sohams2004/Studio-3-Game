cdd62c244125a5010e838e8f625aee888ef284d9
Soham Shinde <sohamsshinde04@gmail.com>
2024-07-16 09:55:06 +0400

    Fixes

--- Assets/Scripts/ObjectPickUp.cs
+++ Assets/Scripts/ObjectPickUp.cs
@@ -520,8 +520,8 @@ public class ObjectPickUp : MonoBehaviour
     private void Update()
     {
         ObjectDetect();
-        PaperNote();
         PlaceObjects();
+        PaperNote();
         /* OpenDoor();*/
         /*BlindsOpen();*/
         Money();
