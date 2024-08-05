7aa49f8c377bd351b27609e1f64fd47c05607219
Aly Sharaf <ali.charaf59@gmail.com>
2024-08-05 12:36:50 +0400

    can interact with the clock to play voiceline

---
+++ Assets/Ali/Scripts/BigClock.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BigClock : MonoBehaviour
+{
+     public float interactionDistance = 3f; // Adjust as necessary
+    public Transform interactionText; // Assign the 3D Text transform in the Inspector
+    public LayerMask interactableLayer; // Assign the layer of interactable objects
+
+    void Start()
+    {
+        interactionText.gameObject.SetActive(false); // Hide the interaction text initially
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactionDistance, interactableLayer))
+        {
+            if (hit.collider.CompareTag("Clock"))
+            {
+                interactionText.gameObject.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    // Play the interaction sound from the clock
+                    AudioSource audioSource = hit.collider.GetComponent<AudioSource>();
+                    if (audioSource != null)
+                    {
+                        audioSource.Play();
+                    }
+                    // Additional interaction logic can go here
+                }
+            }
+            else
+            {
+                interactionText.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            interactionText.gameObject.SetActive(false);
+        }
+    }
+}
