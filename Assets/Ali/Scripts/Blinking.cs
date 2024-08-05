1b405134c5e6b927659c2a07f88260c4694c1025
Aly Sharaf <ali.charaf59@gmail.com>
2024-08-05 12:14:46 +0400

    Ai voices for parents added to cutscene

--- Assets/Ali/Scripts/Blinking.cs
+++ Assets/Ali/Scripts/Blinking.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public class Blinking : MonoBehaviour
 {
-     public Animator animator; // Reference to the Animator component
-    public string parameterName; // The name of the parameter to trigger the animation
-    public float delay = 5f; // Time in seconds before playing the animation (default to 5 seconds)
+     public Animator animator; 
+    public string parameterName; 
+    public float delay = 5f; 
 
     void Start()
     {
-        // Start the coroutine
+        
         StartCoroutine(PlayAnimationAfterDelayCoroutine());
     }
 
     IEnumerator PlayAnimationAfterDelayCoroutine()
     {
-        Debug.Log("Waiting for delay...");
-        // Wait for the specified delay
         yield return new WaitForSeconds(delay);
 
-        Debug.Log("Triggering animation after delay...");
-        // Trigger the animation using the parameter
         animator.SetTrigger(parameterName);
     }
 }
